998994c611074f6ab40020deade0e961aaa869e6
Zhou Chenkai <2412086080@qq.com>
2024-12-29 16:23:20 +0800

    feat:v4

--- openai-code-review-test/src/test/java/com/openai/code/review/OpenAICodeReviewTest.java
+++ openai-code-review-test/src/test/java/com/openai/code/review/OpenAICodeReviewTest.java
@@ -16,6 +16,7 @@ public class OpenAICodeReviewTest {
         System.out.println("111");
         System.out.println("222");
         System.out.println("333");
+        System.out.println("444");
     }
 
 }
