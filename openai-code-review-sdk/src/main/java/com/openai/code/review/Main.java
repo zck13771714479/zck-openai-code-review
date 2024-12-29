a45e0c7d9ee32f9cf0c65f0cc2a01559b749c6b0
Zhou Chenkai <2412086080@qq.com>
2024-12-29 16:13:44 +0800

    feat:maven构建评审sdk

--- openai-code-review-sdk/src/main/java/com/openai/code/review/Main.java
+++ openai-code-review-sdk/src/main/java/com/openai/code/review/Main.java
@@ -1,7 +1,27 @@
 package com.openai.code.review;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("this is OpenAI Code Review SDK!!!");
+
+        ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "HEAD~1", "HEAD");
+        processBuilder.directory(new File("."));
+        Process process = processBuilder.start();
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+        StringBuilder stringBuilder = new StringBuilder();
+        while ((line = bufferedReader.readLine()) != null) {
+            stringBuilder.append(line);
+        }
+        int exitValue = process.waitFor();
+        System.out.println("Exit value: " + exitValue);
+        System.out.println("Git diff " + stringBuilder.toString());
+        bufferedReader.close();
     }
 }
