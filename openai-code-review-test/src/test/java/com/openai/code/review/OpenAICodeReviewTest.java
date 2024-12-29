7b30cff647b0b37f8d39ef0b83714351347836d2
Zhou Chenkai <2412086080@qq.com>
2024-12-29 16:20:45 +0800

    feat:v2

---
+++ openai-code-review-test/src/test/java/com/openai/code/review/OpenAICodeReviewTest.java
@@ -0,0 +1,21 @@
+package com.openai.code.review;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@Slf4j
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class OpenAICodeReviewTest {
+
+    @Test
+    public void test(){
+        System.out.println("111");
+        System.out.println("222");
+        System.out.println("333");
+    }
+
+}
