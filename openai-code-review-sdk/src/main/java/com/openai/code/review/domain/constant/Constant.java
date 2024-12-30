b25cef887a3719d6e4b4aed2cbd3360e2c685a48
Zhou Chenkai <2412086080@qq.com>
2024-12-30 16:23:26 +0800

    fix:修改正确git仓库地址

--- openai-code-review-sdk/src/main/java/com/openai/code/review/domain/constant/Constant.java
+++ openai-code-review-sdk/src/main/java/com/openai/code/review/domain/constant/Constant.java
@@ -3,5 +3,5 @@ package com.openai.code.review.domain.constant;
 public class Constant {
     public static final String API_KEY = "e9d317d6c65a1bb087cc653ef3dd9884.J75wUkQz21iPZQJL";
     public static final String API_URL = "https://open.bigmodel.cn/api/paas/v4/chat/completions";
-    public static final String LOG_REPOSITORY_URI = "";
+    public static final String LOG_REPOSITORY_URI = "https://github.com/zck13771714479/zck-openai-code-review-logs.git";
 }
